e0d0fa61d84d41673fe5600e131513c5967eab00
mgyoung77 <young_michael@bah.com>
2016-05-23 21:42:09 -0400

    Update cachematrix.R
    Mike Young's matrix caching function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function will determin the inverse of a matrix
+## if the matrix was previously solved, itthe function will
+## will recall the previous answer to improve processing time
 
-## Write a short comment describing this function
+## creates a list set of functions for setting, assigning,
+## solving and recalling the matrix and inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix(ncol = 2)){
+  inv <- NULL #initizes inverse of matrix as null
+  set <- function(m) { #sets matrix and inverse
+    mat <<- m #mat gets assigned whatever the m is
+    inv <<- NULL #sets the inverse to NULL since it assumes we are reseting
+  }
+  get <- function() mat #gets the matrix
+  getInv <- function () inv #gets the inverse of matrix
+  setInv <- function(x) inv <<- x #sets the inverse of matrix with 'x'
+  list (set = set, get = get,
+        getInv = getInv,
+        setInv = setInv) #returns a list of the functions within the parent function for use later
 }
 
 
-## Write a short comment describing this function
+## this function calls the list function of makeCacheMatrix with an assigned matrix
+## and will determine if the inverse has already been solved and cached
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matri, ...) {
+  inver <- matri$getInv() #gets the invserse, the first time it will be NULL
+  if(!is.null(inver)) { #if the inverse of matrix is not null (meaning it was cached)
+    message("getting cached matrix data")
+    return(inver) #returns the inverse without solving it since already stored
+  }
+  d <- matri$get() #gets the matrix data
+  inver <- solve(d,...) #solves the matrix information
+  matri$setInv(inver) #sets the inv to the 'inv' directly above
+  inver
 }
